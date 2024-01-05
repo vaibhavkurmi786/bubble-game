cd15ec0fbc5b33cc16c7bf7b948712e42be1a90e
vaibhavkurmi <vaibhavkurmi786@gmail.com>
2024-01-05 22:24:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+
+let timer = 60 ;
+let strikeval = 0;
+let rn = 0 ; 
+let score = 0;
+
+function resetGame(){
+    window.location.reload()
+}
+
+function startGame() {
+    
+    
+    function randomNumberGenerator() {
+    
+    let divadd = "";
+    for (let a = 1; a <=126; a++) {
+         rn =  Math.round(Math.random()*10)
+        divadd += `<div class="bubble">${rn}</div>`;
+    
+        
+    }
+    
+    document.querySelector("#pbot").innerHTML = divadd;
+    
+}
+
+
+function timerRem() {
+let tt = setInterval(() => {
+    if(timer>0){
+    timer--;        
+    document.querySelector('#timerval').textContent =timer
+}
+else{
+    clearInterval(tt)
+    document.querySelector('#pbot').innerHTML = `<h1>Game Over</h1>`
+}
+}, 1000)
+}
+
+function strike(){
+    rn = Math.round(Math.random()*10)
+    document.querySelector('#strikeval').textContent = `${rn}`
+}
+
+function scoreIncrease(params) {
+    score += 10
+    document.querySelector("#scoreval").innerText = score;
+}
+
+    document.querySelector('#pbot').addEventListener('click', function(e){
+        let scoreid = Number(e.target.innerText)
+        
+        if(scoreid === rn){
+            
+            scoreIncrease()
+            randomNumberGenerator()
+            strike()
+        }
+    })
+
+
+randomNumberGenerator()
+timerRem();
+strike()
+}
+
